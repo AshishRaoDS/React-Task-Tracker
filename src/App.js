5d4a782df1677d579beff8066b59e8010942d8f8
Ashish Rao D S <ashraods@gmail.com>
2021-11-18 09:10:07 +0530

    production build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react"
+import AddTask from "./components/AddTask";
+
 
 function App() {
+  const [showAddTask,setShowAddTask]=useState(false)
+  const [tasks,setTask]=useState(
+    [
+        {
+            id:1,
+            text:"Meet Tanaya",
+            day:"15th Jan 2022 at 9 am",
+            reminder:true
+        },
+        {
+            id:2,
+            text:"Have breakfast with Tanaya",
+            day:"15th Jan 2022 at 10:30 am",
+            reminder:true
+        },
+        {
+            id:3,
+            text:"Go specs shopping with her",
+            day:"16th Jan 2022 at 4 pm",
+            reminder:false
+        }
+    ]
+)
+
+// Delete task
+const onDelete=(id)=>{
+ setTask(tasks.filter(task=>task.id !==id))
+}
+
+// Toggle reminder
+const toggleReminder=(id)=>{
+  setTask(tasks.map((task)=>
+  task.id===id ? {...task,reminder:!task.reminder} :task))
+}
+
+// Add Task
+const addTask = (task)=>{
+
+  const id=Math.floor(Math.random()*1000)+1;
+  const newTask={id,...task}
+  setTask([newTask,...tasks])
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+     {showAddTask && <AddTask onAdd={addTask}/> }
+     {tasks.length ? <Tasks tasks={tasks} onToggle={toggleReminder} onDelete={onDelete}/> : "No tasks left"}
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,16 @@
+import Task from "./Task"
+
+
+const Tasks = ({tasks, onDelete, onToggle}) => {
+
+    return (
+        <>
+            {
+                tasks.map(task=>(
+                <Task key={task.id} onToggle={onToggle} onDelete={onDelete} task={task} />
+                ))}
+        </>
+    )
+}
+
+export default Tasks
